3f2910d533fa5bda0178e6e6289835dd954a0f25
Paul.Inglsmit <paul.inglsmit@gmail.com>
2024-02-25 18:53:11 +0300

    #2 Send intro info

--- main.go
+++ main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"os"
-
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/joho/godotenv"
+	"log"
+	"os"
+	"time"
 )
 
 var (
@@ -36,6 +36,35 @@ func isStartMessage(update *tgbotapi.Update) bool {
 	return update.Message != nil && update.Message.Text == "/start"
 }
 
+func sendStringMessage(msg string) {
+	_, err := gBot.Send(tgbotapi.NewMessage(gChatId, msg))
+	if err != nil {
+		return
+	}
+}
+func delay(sec uint8) {
+	time.Sleep(time.Second * time.Duration(sec))
+}
+
+func sendMessageWithDelay(delayInSec uint8, message string) {
+	sendStringMessage(message)
+	delay(delayInSec)
+}
+
+func printIntro() {
+	sendMessageWithDelay(2, "Hello! "+EMOJI_SUNGLASSES)
+	sendMessageWithDelay(7, "There are numerous beneficial actions that, by performing regularly, we improve the quality of our life. However, often it's more fun, easier, or tastier to do something harmful. Isn't that so?")
+	sendMessageWithDelay(7, "With greater likelihood, we'll prefer to get lost in YouTube Shorts instead of an English lesson, buy M&M's instead of vegetables, or lie in bed instead of doing yoga.")
+	sendMessageWithDelay(1, EMOJI_SAD)
+	sendMessageWithDelay(10, "Everyone has played at least one game where you need to level up a character, making them stronger, smarter, or more beautiful. It's enjoyable because each action brings results. In real life, though, systematic actions over time start to become noticeable. Let's change that, shall we?")
+	sendMessageWithDelay(1, EMOJI_SMILE)
+	sendMessageWithDelay(14, `Before you are two tables: "Useful Activities" and "Rewards". The first table lists simple short activities, and for completing each of them, you'll earn the specified amount of coins. In the second table, you'll see a list of activities you can only do after paying for them with coins earned in the previous step.`)
+	sendMessageWithDelay(1, EMOJI_COIN)
+	sendMessageWithDelay(10, `For example, you spend half an hour doing yoga, for which you get 2 coins. After that, you have 2 hours of programming study, for which you get 8 coins. Now you can watch 1 episode of "Interns" and break even. It's that simple!`)
+	sendMessageWithDelay(6, `Mark completed useful activities to not lose your coins. And don't forget to "purchase" the reward before actually doing it.`)
+
+}
+
 func main() {
 	log.Printf("Authorized on account %s", gBot.Self.UserName)
 
@@ -48,22 +77,9 @@ func main() {
 		if isStartMessage(&update) {
 			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
 
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-			msg.ReplyToMessageID = update.Message.MessageID
-
-			_, err := gBot.Send(msg)
-			if err != nil {
-				return
-			}
+			gChatId = update.Message.Chat.ID
+			printIntro()
 		}
-		//if update.Message != nil { // If we got a message
-		//	log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-		//
-		//	msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-		//	msg.ReplyToMessageID = update.Message.MessageID
-		//
-		//	gBot.Send(msg)
-		//}
 	}
 
 }
